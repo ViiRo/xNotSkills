8b2ce54a6e8445be7a59e017e3a884eaf5f0e71d
ViiRo <45905915+ViiRo@users.noreply.github.com>
2018-12-20 15:36:13 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("525317384345485322")
+setInterval(function() {
+channel.send(`xNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5raxNotSkills 5ra`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
